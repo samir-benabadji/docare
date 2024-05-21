2e9316167e8867d23917add88fdf1204416393e6
Samir Benabadji <benabadji.samir.2002@gmail.com>
2024-05-21 16:45:45 +0100

    Improving the handling of "connectivity plus".
    Improving the handling of "connectivity plus" and delivering back to the user a meaning message when the internet connection is gone / back.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:async';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,8 +15,50 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late StreamSubscription<ConnectivityResult> subscription;
+  bool _previouslyConnected = true;
+
+  @override
+  void initState() {
+    super.initState();
+    subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      bool isConnected = result != ConnectivityResult.none;
+
+      if (!isConnected && _previouslyConnected) {
+        Get.snackbar(
+          "Connection Lost",
+          "You have lost internet connection",
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      } else if (isConnected && !_previouslyConnected) {
+        Get.snackbar(
+          "Connection Restored",
+          "You are back online",
+          backgroundColor: Colors.green,
+          colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      }
+
+      _previouslyConnected = isConnected;
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
