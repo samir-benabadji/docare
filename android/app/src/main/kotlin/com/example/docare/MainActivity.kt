05c4e0fa852769c1734e5652c487d95b587e6018
Samir Benabadji <benabadji.samir.2002@gmail.com>
2024-05-21 16:58:10 +0100

    Disabling taking screenshots / videos on both android and iOS as a security measure for our app.
    Disabling taking screenshots / videos on both android and iOS as a security measure for our app.

--- android/app/src/main/kotlin/com/example/docare/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/docare/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.docare
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,28 @@
 import UIKit
 import Flutter
 
+class SecureWindow: UIWindow {
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        if #available(iOS 13.0, *) {
+            self.isSecure = true
+        }
+    }
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  override var window: UIWindow? {
+      get {
+          return super.window
+      }
+      set {
+          let secureWindow = SecureWindow(frame: UIScreen.main.bounds)
+          secureWindow.rootViewController = newValue?.rootViewController
+          super.window = secureWindow
+      }
+  }
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
